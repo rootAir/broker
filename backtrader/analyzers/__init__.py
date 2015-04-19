c4b593e39ffb66f3214ea3d6721df5a1ccf3ac79
Luiz <junqueira.luizcarlo@gmail.com>
2015-04-18 23:15:57 -0300

    Keeps track of the life of an trade: size, price,

---
+++ backtrader/analyzers/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# The modules below should/must define __all__ with the objects wishes
+# or prepend an "_" (underscore) to private classes/variables
+
+from .annualreturn import *
+from .sharpe import *
+from .tradeanalyzer import *
+from .sqn import *
