df64a7f339782801fbeab9190dececff9f563f8b
Luiz <junqueira.luizcarlo@gmail.com>
2015-04-23 23:16:49 -0300

    - The ``dataname`` parameter is a blaze ``Data`` object

---
+++ backtrader/feeds/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+from .csvgeneric import *
+from .btcsv import *
+from .vchartcsv import *
+from .vchart import *
+from .yahoo import *
+from .sierrachart import *
+from .pandafeed import *
