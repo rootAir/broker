95ea0dfa2b0fd8b6288408810da5798f92c6aa22
Luiz <junqueira.luizcarlo@gmail.com>
2015-05-03 23:18:52 -0300

    Records the "true high" which is the maximum of today's high and

---
+++ backtrader/indicators/bollinger.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from . import Indicator, MovAv, StdDev
+
+
+class BollingerBands(Indicator):
+    '''
+    Defined by John Bollinger in the 80s. It measures volatility by defining
+    upper and lower bands at distance x standard deviations
+
+    Formula:
+      - midband = SimpleMovingAverage(close, period)
+      - topband = midband + devfactor * StandardDeviation(data, period)
+      - botband = midband - devfactor * StandardDeviation(data, period)
+
+    See:
+      - http://en.wikipedia.org/wiki/Bollinger_Bands
+    '''
+    alias = ('BBands',)
+
+    lines = ('mid', 'top', 'bot',)
+    params = (('period', 20), ('devfactor', 2.0), ('movav', MovAv.Simple),)
+
+    plotinfo = dict(subplot=False)
+    plotlines = dict(
+        mid=dict(ls='--'),
+        top=dict(_samecolor=True),
+        bot=dict(_samecolor=True),
+    )
+
+    def _plotlabel(self):
+        plabels = [self.p.period, self.p.devfactor]
+        plabels += [self.p.movav] * self.p.notdefault('movav')
+        return plabels
+
+    def __init__(self):
+        self.lines.mid = ma = self.p.movav(self.data, period=self.p.period)
+        stddev = self.p.devfactor * StdDev(self.data, ma, period=self.p.period)
+        self.lines.top = ma + stddev
+        self.lines.bot = ma - stddev
+
+        super(BollingerBands, self).__init__()
